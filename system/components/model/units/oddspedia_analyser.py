cca14e6f9118b4a3a9e5befa2df220023f0b7108
kvenkatesan <k-venkatesan@outlook.com>
2024-01-01 15:12:05 +0100

    Add black code formatting tool

---
+++ system/components/model/units/oddspedia_analyser.py
@@ -0,0 +1,7 @@
+class OddspediaAnalyser:
+    def __init__(self):
+        self._initialisation_string = "Oddspedia Analyser initialising"
+
+    def get_initialisation_string(self):
+        return self._initialisation_string
+    
