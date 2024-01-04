d36ab5a44112c365be75a406dbfd8eafe09b2117
kvenkatesan <k-venkatesan@outlook.com>
2024-01-04 14:57:18 +0100

    Add comment regarding deviation from PEP-8 to assertion_helpers.py

--- system/component_tests/utilities/assertion_helpers.py
+++ system/component_tests/utilities/assertion_helpers.py
@@ -1,3 +1,7 @@
+# Note: Function names are chosen here to match with Python's unittest library
+# and may therefore deviate from PEP-8 standards
+
+
 def assertEqual(expected, actual):
     if expected != actual:
         raise Exception(
