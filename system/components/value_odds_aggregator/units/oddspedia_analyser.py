517ada4c8cde05d4acc51efb97fcaed98b44adfe
kvenkatesan <k-venkatesan@outlook.com>
2024-01-08 21:59:47 +0100

    Fix code formatting to comply with black standards

--- system/components/value_odds_aggregator/units/oddspedia_analyser.py
+++ system/components/value_odds_aggregator/units/oddspedia_analyser.py
@@ -2,6 +2,5 @@ class OddspediaAnalyser:
     def __init__(self):
         self._initialisation_string = "Oddspedia Analyser initialising"
 
-
     def get_initialisation_string(self):
         return self._initialisation_string
