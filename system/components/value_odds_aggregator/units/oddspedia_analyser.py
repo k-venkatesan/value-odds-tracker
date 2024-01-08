d18ee69b8221cbf6409da6623d2c16af9ec88390
kvenkatesan <k-venkatesan@outlook.com>
2024-01-08 21:55:30 +0100

    Introduce temporary code formatting error to test detection

--- system/components/value_odds_aggregator/units/oddspedia_analyser.py
+++ system/components/value_odds_aggregator/units/oddspedia_analyser.py
@@ -2,5 +2,6 @@ class OddspediaAnalyser:
     def __init__(self):
         self._initialisation_string = "Oddspedia Analyser initialising"
 
+
     def get_initialisation_string(self):
         return self._initialisation_string
